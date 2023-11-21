f600cf43a6acdfd296e2327e4b777d47e539e903
TK <Tauqeer.khan@student.curtin.edu.au>
2023-11-21 04:06:21 +0400

    made changes in calculateArea to follow figure 2.

--- calculateArea.java
+++ calculateArea.java
@@ -2,15 +2,27 @@ public class calculateArea
 {
     public static double calculateArea(double sideLength, int sides)
     {
-        if (sideLength <= 0 || sides < 3)
+        switch (sides)
         {
-            throw new IllegalArgumentException("Invalid input : Enter positive sideLength and sides must be atleast 3.");
-        }
-     
-        double perimeter = CalculatePerimeter.calculatePerimeter(sideLength, sides);
+            case 3: //Triangle
+                return (Math.sqrt(3) / 4) * sideLength * sideLength;
+            break;
 
-        double apothem = sideLength / (2 * Math.tan(Math.PI / sides));  
-        return (perimeter * apothem) / 2;
+            case 4: // Square
+                area = Math.pow(sideLength, 2);
+            break;
+
+            case 5: // Pentagon
+            case 6: // Hexagon
+            case 7: // Heptagon
+                area = (sides * Math.pow(sideLength, 2)) / (4 * Math.tan(Math.PI / sides));
+                break;
+            default: 
+                throw new IllegalArgumentException("Invalid number of sides");
+
+        }
+        return area;
     }
+                            
 }
     
