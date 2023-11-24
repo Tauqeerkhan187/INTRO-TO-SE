9804f0b97a839b2135f7b01010245fa2723f0a8d
TK <Tauqeer.khan@student.curtin.edu.au>
2023-11-24 16:08:29 +0400

    Made blackbox testing methods for PolygonCalculator App.

---
+++ PolygonCalculatorAppTestBlackBoxTests.java
@@ -0,0 +1,45 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class PolygonCalculatorAppBlackBoxTests
+{
+    @Test
+    public void testIdentifyTriangle()
+    {
+        assertEquals("Triangle",IdentifyShape.identifyShape(3));
+    }
+
+    @Test
+    public void testPerimeterCalculation()
+    {
+        assertEquals(15.0, CalculatePerimeter.calculatePerimeter(5.0, 3), 0.001);
+    }
+
+    @Test
+    public void testAreaCalculation()
+    {
+        assertEquals(10.825, CalculateArea.calculateArea(5.0, 3), 0.001);
+    }
+
+    @Test
+    public void testHandSizeComparisonWithinRange()
+    {
+        String result = PolygonCalculatorApp.compareAreaWithHandSize(80);
+        assertEquals("The area is close to the size of a child's hand.", result);
+    }
+
+    @Test
+    public void testHandSizeComparisonBelowRange()
+    {
+        String result = PolygonCalculatorApp.compareAreaWithHandSize(70.0);        assertEquals("The area is smaller than a child's hand size.", result);
+    }
+
+    @Test
+    public void testHandsizeComparisonAboveRange()
+    {
+        String result = PolygonCalculatorApp.compareAreaWithHandSize(91.0);
+        assertEquals("The area is larger than a child's hand size.", result);
+    }
+}
+    
