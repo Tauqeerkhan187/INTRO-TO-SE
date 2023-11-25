25bf65772455b01434ff8220c946cffefe89ce78
TK <Tauqeer.khan@student.curtin.edu.au>
2023-11-25 18:45:37 +0400

    refractored CalculateArea to add Illegal Exception Argument to run tests smoothly.

--- src/main/java/org/junit/jupiter/CalculateArea.java
+++ src/main/java/org/junit/jupiter/CalculateArea.java
@@ -3,12 +3,20 @@ package org.junit.jupiter;
 public class CalculateArea
 {
     public static double calculateArea(double sideLength, int numberOfSides)
-    {
+    {   
+        if (sideLength <= 0 || numberOfSides < 3)
+        {
+            throw new IllegalArgumentException("Side length and number of sides must be positive");
+        }
+        
         if (numberOfSides == 4)
-        { 
+        {
             return Math.pow(sideLength, 2);
         }
-        return (numberOfSides * Math.pow(sideLength, 2)) / (4 * Math.tan(Math.PI / numberOfSides));
+        else
+        {
+            return (numberOfSides * Math.pow(sideLength, 2)) / (4 * Math.tan(Math.PI / numberOfSides));
+        }
     }
 }
         
