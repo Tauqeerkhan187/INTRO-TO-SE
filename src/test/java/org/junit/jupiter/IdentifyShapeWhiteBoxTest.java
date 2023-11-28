d0dad376fa70c893396dc775a488b1ecb1f7faef
TK <Tauqeer.khan@student.curtin.edu.au>
2023-11-29 03:48:30 +0400

    made test cases for scenario A for shape identification.

---
+++ src/test/java/org/junit/jupiter/IdentifyShapeWhiteBoxTest.java
@@ -0,0 +1,56 @@
+package org.junit.jupiter;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class IdentifyShapeWhiteBoxTest
+{
+    @Test
+    public void testIdentifyTriangle()
+    {
+        assertEquals("Triangle", IdentifyShape.identifyShape(3));
+    }
+
+    @Test
+    public void testIdentifySquare()
+    {
+        assertEquals("Square", IdentifyShape.identifyShape(4));
+    }
+
+    @Test
+    public void testIdentifyPentagon()
+    {
+        assertEquals("Pentagon", IdentifyShape.identifyShape(5));
+    }
+
+    @Test
+    public void testIdentifyHexagon()
+    {
+        assertEquals("Hexagon", IdentifyShape.identifyShape(6));
+    }
+
+    @Test
+    public void testIdentifyHeptagon()
+    {
+        assertEquals("Heptagon", IdentifyShape.identifyShape(7));
+    }
+
+    @Test
+    public void testIdentifyUnknownShapeNegative()
+    {
+        assertEquals("Unknown", IdentifyShape.identifyShape(8));
+    }
+
+    @Test
+    public void testIdentifyUnknownShapeNegative()
+    {
+        assertEquals("Unknown", IdentifyShape.identifyShape(-1));
+    }
+
+    @Test
+    public void testIdentifyUnknownShapeZero()
+    {
+        assertEquals("Unknown", IdentifyShape.identifyShape(0));
+    }
+
+}
